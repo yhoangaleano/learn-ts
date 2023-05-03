932b54720bad5be5f89727d5a9e2eb0e338ac81d
yhoangaleano <yhoangaleano@gmail.com>
2023-05-02 21:30:28 -0500

    Se realizan los ejericios base de los tipos en ts

--- src/basics/exercise-1/app.ts
+++ src/basics/exercise-1/app.ts
@@ -1,6 +1,52 @@
 const teacher = {
     name: 'Yhoan',
-    lastName: 'Galeano'
+    lastName: 'Galeano',
+    age: 29
 };
 
-console.log('Teacher:', teacher);
+console.log('Teacher is json:', teacher);
+
+// Primitivos
+// Booleans
+// Declaración implícita
+const isTeacher = true; // Toma el valor con el cual fue declarado y ese es el tipo
+let isPlayer = true; // Tomo el tipo del valor que le fue asignado
+isPlayer = false;
+
+// Declaración explicita
+const hasErrors: boolean = false;
+let isChampion: boolean = false;
+
+let result = hasErrors ? 'Tiene errores' : 'No tiene errores';
+
+// String
+const firstName = 'Yhoan';
+const lastName = "Galeano";
+
+let fullName: string = `Su nombre es: ${firstName} ${lastName}, bienvenido`;
+console.log(fullName.toUpperCase());
+
+// Number
+let countStudents: number = 29;
+let average: number = 4.5;
+
+console.log( average.toFixed() )
+
+
+// Array
+let fruits = ['pera', 'banano'];
+let sports: string[] = [ 'futbol', 'baloncesto' ];
+
+type stringOrNull = string | null;
+
+// Union types
+let students: Array<stringOrNull> = ['Nico', 'Yhoan', null, 'Rayffer', null, 'Ana'];
+console.log( students.filter((student) => student !== null ) );
+
+// Tuplas
+let exampleTuple: [string, null] = [ 'hola', null ];
+
+// Tipos personalizados
+type statusCode = 'active' | 'inactive';
+
+let studentStatus: statusCode = 'inactive';
