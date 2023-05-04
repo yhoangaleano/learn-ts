3a03aeba028836b4d40b3daf5af97921bbf4cdf0
yhoangaleano <yhoangaleano@gmail.com>
2023-05-03 19:40:05 -0500

    Uso de enums, unknown, typeof

--- src/basics/exercise-1/app.ts
+++ src/basics/exercise-1/app.ts
@@ -30,23 +30,96 @@ console.log(fullName.toUpperCase());
 let countStudents: number = 29;
 let average: number = 4.5;
 
-console.log( average.toFixed() )
+console.log(average.toFixed())
 
 
 // Array
 let fruits = ['pera', 'banano'];
-let sports: string[] = [ 'futbol', 'baloncesto' ];
+let sports: string[] = ['futbol', 'baloncesto'];
 
 type stringOrNull = string | null;
 
 // Union types
 let students: Array<stringOrNull> = ['Nico', 'Yhoan', null, 'Rayffer', null, 'Ana'];
-console.log( students.filter((student) => student !== null ) );
+console.log(students.filter((student) => student !== null));
 
 // Tuplas
-let exampleTuple: [string, null] = [ 'hola', null ];
+let exampleTuple: [string, null] = ['hola', null];
 
 // Tipos personalizados
 type statusCode = 'active' | 'inactive';
 
 let studentStatus: statusCode = 'inactive';
+
+// Any
+let working: any = 25;
+working = 'hola';
+
+// Funciones
+/**
+ * function nombreFuncion(parametro: tipoDato): tipoDatoQueRetornaLaFuncion { }
+ */
+function validateValue(value: unknown): boolean {
+
+    // unknown que es un tipo de dato que es desconocido
+    // para cualquier validación que deba hacer con el dato
+    // debo primero saber o validar que tipo de dato es
+    if (typeof value === 'string') {
+        console.log(value.trim().toUpperCase());
+    } else if (typeof value === 'number') {
+        console.log(value.toFixed());
+    }
+
+    if (value === '') {
+        return false;
+    } else {
+        return true;
+    }
+
+}
+
+function getFullName(firstName: string, lastName: string): string {
+
+    return `${firstName} ${lastName}`;
+
+}
+
+// Void
+function processResponse( saved: boolean ): void {
+
+    if (saved) {
+        console.log('Guardado');
+    } else {
+        console.log('Ocurrió un error');
+    }
+
+}
+
+
+console.log( getFullName(firstName, lastName)  );
+processResponse(true);
+
+
+enum Role {
+    Admin,
+    Client,
+    Users,
+    SuperAdmin
+}
+
+let users: Array< { name: string, role: Role } > = [
+    {
+        name: 'Pepito',
+        role: Role.Admin
+    },
+    {
+        name: 'Juanito',
+        role: Role.Client
+    },
+    {
+        name: 'Fulanito',
+        role: Role.Client
+    }
+]
+
+console.log( users.filter( (user) => user.role === Role.Client  ) );
